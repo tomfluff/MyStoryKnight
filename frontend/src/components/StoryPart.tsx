d3aa31e5952af4843e0e878f6d04d0de9f81916c
Yotam <92244828+tomfluff@users.noreply.github.com>
2024-03-03 11:47:28 +0900

    Refactor StoryView component and add load more button

--- frontend/src/components/StoryPart.tsx
+++ frontend/src/components/StoryPart.tsx
@@ -4,7 +4,6 @@ import {
   Box,
   Flex,
   Paper,
-  Skeleton,
   useMantineColorScheme,
   Avatar,
   Group,
--- frontend/src/components/StoryView.tsx
+++ frontend/src/components/StoryView.tsx
@@ -1,8 +1,6 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useMemo, useRef, useState } from "react";
 import {
-  Grid,
   Flex,
-  Text,
   Skeleton,
   Box,
   Group,
@@ -10,41 +8,49 @@ import {
   Button,
   Stack,
 } from "@mantine/core";
-import { useScrollIntoView } from "@mantine/hooks";
+import { useScrollIntoView, useCounter } from "@mantine/hooks";
 import StoryPart from "./StoryPart";
 
 type Props = {};
 
 const StoryView = (props: Props) => {
-  const { targetRef, scrollIntoView } = useScrollIntoView<HTMLDivElement>();
+  const { targetRef, scrollIntoView } = useScrollIntoView<HTMLDivElement>({
+    duration: 250,
+  });
+  const [count, { increment }] = useCounter(0);
 
   useEffect(() => {
-    scrollIntoView();
-  }, []);
+    scrollIntoView({
+      alignment: "start",
+    });
+  }, [count]);
 
   return (
-    <Group align="center" justify="center">
-      <Flex
-        w={{ xs: "100%", sm: "80%", md: "70%" }}
-        direction="column"
-        justify="flex-end"
-        gap="sm"
-      >
-        <Stack>
-          {Array(20)
-            .fill(0)
-            .map((_, i) => (
-              <StoryPart key={i} />
-            ))}
-        </Stack>
-        <Paper ref={targetRef}>
-          <Group justify="center">
-            <Skeleton height={64} w={128} />
-            <Skeleton height={64} w={128} />
-          </Group>
-        </Paper>
-      </Flex>
-    </Group>
+    <Box>
+      <Group align="center" justify="center">
+        <Flex
+          w={{ xs: "100%", sm: "80%", md: "70%" }}
+          direction="column"
+          justify="flex-end"
+          gap="sm"
+        >
+          <Stack>
+            {Array(count)
+              .fill(0)
+              .map((_, i) => (
+                <StoryPart key={i} />
+              ))}
+          </Stack>
+          <Paper>
+            <Group justify="center" ref={targetRef}>
+              <Skeleton height={64} w={128} />
+              <Skeleton height={64} w={128} />
+              <Button onClick={increment}>Load more</Button>
+            </Group>
+          </Paper>
+        </Flex>
+      </Group>
+    </Box>
   );
 };
 
