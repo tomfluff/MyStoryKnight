ec63d6b3e7020a56cce3c06f537528a7510cb999
tomfluff <yotsurfing@gmail.com>
2024-03-05 18:02:15 +0900

    save traffic by replacing useQuary with useMutation

--- frontend/src/components/ReadController.tsx
+++ frontend/src/components/ReadController.tsx
@@ -9,9 +9,10 @@ import {
   rem,
 } from "@mantine/core";
 import { useCounter, useDisclosure } from "@mantine/hooks";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import getAxiosInstance from "../utils/axiosInstance";
 import { FaPause, FaPlay, FaStop } from "react-icons/fa";
+import { FaRotateLeft } from "react-icons/fa6";
 
 type Props = {
   id?: string;
@@ -24,150 +25,149 @@ const ReadController = ({ id, text, autoPlay }: Props) => {
   const instance = getAxiosInstance();
   const audioRef = useRef<HTMLAudioElement>(new Audio());
   const readerRef = useRef<ReadableStreamDefaultReader<Uint8Array>>();
-  const [connection, { open: openConnection, close: closeConnection }] =
-    useDisclosure(false);
 
   const [playing, { open, close }] = useDisclosure(false, {
     onOpen: () => {
-      if (audioRef.current && audioRef.current.paused) {
-        audioRef.current.play();
-      }
+      if (!speech.data) speech.mutate(text);
+      else if (audioRef.current) audioRef.current.play();
     },
     onClose: () => {
-      if (audioRef.current && !audioRef.current.paused) {
-        audioRef.current.pause();
-        audioRef.current.currentTime = 0;
-      }
+      console.log("onClose")
+      if (audioRef.current) audioRef.current.pause();
     },
   });
 
-  const read = async (text: string, signal: AbortSignal) => {
+  const reset = () => {
+    if (audioRef.current) {
+      audioRef.current.currentTime = 0;
+    }};
+
+  const read = async (text: string) => {
     const response = await fetch(`${instance.defaults.baseURL}/read`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
         text: text,
       }),
-      signal: signal,
     });
     return response.body;
   };
 
-  const {
-    data: audioStream,
-    isError,
-    isLoading,
-    error,
-  } = useQuery({
-    queryKey: ["audio", { id, text }],
-    queryFn: ({ signal }) => read(text, signal),
-    enabled: !!text,
-    refetchOnMount: true,
-    refetchOnWindowFocus: false,
+  const speech = useMutation({
+    mutationKey: ["audio"],
+    mutationFn: (text: string) => read(text),
+    onSuccess: (data) => {
+      const audioStream = data;
+      console.log("audioStream", audioStream);
+      if (!audioStream?.locked) readerRef.current = audioStream?.getReader();
+
+      if (audioRef.current && readerRef.current) {
+        const mediaSource = new MediaSource();
+        audioRef.current.src = URL.createObjectURL(mediaSource);
+        audioRef.current.oncanplay = () => {
+          audioRef.current.play();
+        };
+
+        mediaSource.addEventListener(
+          "sourceopen",
+          () => {
+            const sourceBuffer = mediaSource!.addSourceBuffer("audio/mpeg");
+            sourceBuffer.mode = "sequence";
+
+            // Update source buffer when it's ready for more data
+            sourceBuffer.addEventListener("updateend", () => {
+              if (!sourceBuffer?.updating) {
+                readerRef.current!.read().then(({ done, value }) => {
+                  if (done) {
+                    mediaSource!.endOfStream();
+                    return;
+                  }
+                  appendChunk(sourceBuffer, value);
+                });
+              }
+            });
+
+            // Initial read
+            readerRef.current!.read().then(({ done, value }) => {
+              if (!done) {
+                appendChunk(sourceBuffer, value);
+              }
+            });
+          },
+          { once: true }
+        );
+      }
+    },
+    onError: (error) => {
+      increment();
+    },
   });
 
   const appendChunk = (sourceBuffer: SourceBuffer, value: Uint8Array) => {
     try {
       sourceBuffer.appendBuffer(value);
-      closeConnection();
     } catch (e) {
       console.error(errorCnt, e);
       increment();
     }
   };
 
+
   useEffect(() => {
-    openConnection();
-    if (!audioStream?.locked) readerRef.current = audioStream?.getReader();
-
-    if (audioRef.current && readerRef.current) {
-      const mediaSource = new MediaSource();
-      audioRef.current.src = URL.createObjectURL(mediaSource);
-
-      mediaSource.addEventListener(
-        "sourceopen",
-        () => {
-          const sourceBuffer = mediaSource!.addSourceBuffer("audio/mpeg");
-          sourceBuffer.mode = "sequence";
-
-          // Update source buffer when it's ready for more data
-          sourceBuffer.addEventListener("updateend", () => {
-            if (!sourceBuffer?.updating) {
-              readerRef.current!.read().then(({ done, value }) => {
-                if (done) {
-                  mediaSource!.endOfStream();
-                  return;
-                }
-                appendChunk(sourceBuffer, value);
-              });
-            }
-          });
-
-          // Initial read
-          readerRef.current!.read().then(({ done, value }) => {
-            if (!done) {
-              appendChunk(sourceBuffer, value);
-            }
-          });
-        },
-        { once: true }
-      );
-    }
-  }, [audioStream]);
-
-  const cleanup = useCallback(() => {
-    console.log("cleanup", text);
-    if (audioRef.current) {
-      audioRef.current.pause();
-      audioRef.current.src = "";
-    }
-    if (readerRef.current && !readerRef.current.closed) {
-      readerRef.current.cancel();
-      readerRef.current.releaseLock();
-      readerRef.current = undefined;
-    }
+    return () => {
+      console.log("cleanup", text);
+      if (audioRef.current) {
+        audioRef.current.pause();
+        audioRef.current.src = "";
+      }
+      if (readerRef.current && !readerRef.current.closed) {
+        readerRef.current.cancel();
+        readerRef.current.releaseLock();
+        readerRef.current = undefined;
+      }
+    };
   }, [text]);
 
   useEffect(() => {
-    return () => cleanup();
-  }, [text]);
+    console.log("autoPlay", autoPlay)
+    if (autoPlay) {
+      open();
+    }
+  }, [autoPlay]);
 
   return (
     <Group justify="space-between" align="center">
-      <Tooltip label="Click to read" position="bottom" withArrow>
-        {isError ? (
-          <Badge size="xs" color="red">
-            Error: {error?.message}
-          </Badge>
-        ) : (
-          <Group gap="xs">
+      {speech.isError ? (
+        <Badge size="xs" color="red">
+          Error: {speech.error?.message}
+        </Badge>
+      ) : (
+        <Group gap="xs">
+          <Button
+            variant="filled"
+            size="xs"
+            radius="xl"
+            onClick={playing ? close : open}
+            color={"gray"}
+          >
+            {playing ? <FaPause /> : <FaPlay />}
+          </Button>
+          {speech.isSuccess ? (
             <Button
               variant="filled"
               size="xs"
               radius="xl"
-              disabled={connection || audioStream === undefined}
-              onClick={playing ? () => audioRef.current?.pause() : open}
-              color={"gray"}
+              color="gray"
+              disabled={!playing}
+              onClick={reset}
             >
-              {playing ? <FaPause /> : <FaPlay />}
+              <FaRotateLeft />
             </Button>
-            {audioStream ? (
-              <Button
-                variant="filled"
-                size="xs"
-                radius="xl"
-                color="red"
-                disabled={connection || audioStream === undefined}
-                onClick={close}
-              >
-                <FaStop />
-              </Button>
-            ) : null}
-            {isLoading ? <Loader size="xs" color="gray.3" /> : null}
-          </Group>
-        )}
-      </Tooltip>
-      <audio ref={audioRef} autoPlay={autoPlay} onPlay={open} onPause={close} />
+          ) : null}
+          {speech.isPending ? <Loader size="xs" color="gray.3" /> : null}
+        </Group>
+      )}
+      <audio ref={audioRef} onPlay={open} onPause={close} />
     </Group>
   );
 };
