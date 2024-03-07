83d5cd9909e188e93647fcd0e7db83a66f984e72
Yotam <92244828+tomfluff@users.noreply.github.com>
2024-03-08 01:06:33 +0900

    Update deployment command in package.json

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,14 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/MyStoryKnight/",
-  plugins: [react()],
-  preview: {
-    port: 3000,
-    strictPort: true,
-  },
-  server: {
-    port: 3000,
-    strictPort: true,
-    host: true,
-  },
 });
