12caa2196670beca5d44596b2286d4135c7483dc
Yotam <92244828+tomfluff@users.noreply.github.com>
2024-03-08 00:57:15 +0900

    Update homepage URL and remove unused code

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { NotFoundTitle } from "./components/NotFoundTitle/NotFoundTitle.tsx";
 import "./index.css";
 
 // Import styles of packages that you've installed.
@@ -13,17 +12,6 @@ const theme = createTheme({
   primaryColor: "violet",
 });
 
-// Import `createBrowserRouter` and `RouterProvider` from `react-router-dom`
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <NotFoundTitle />,
-  },
-]);
-
 // Import `QueryClientProvider` and `QueryClient` from `@tanstack/react-query`
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
@@ -34,7 +22,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <MantineProvider theme={theme} defaultColorScheme="dark">
       <QueryClientProvider client={queryClient}>
-        <RouterProvider router={router} />
+        <App />
         <ReactQueryDevtools />
       </QueryClientProvider>
     </MantineProvider>
